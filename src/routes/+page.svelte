915383172458f1881ee441f0a2694549d0e386e0
zummon <zummon.space@gmail.com>
2024-05-04 14:27:24 +0700

    save

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,6 @@
 	<meta name="description" content={data.description} />
 </svelte:head>
 
-<svelte:body style={`background-image: url(${data.cover.src});`}></svelte:body>
-
 <div class="bg-white bg-opacity-75 hover:bg-opacity-100 transition ease-in-out duration-500 rounded-lg max-w-screen-md mx-auto p-4 mb-4 text-center">
 	<h1 class="text-3xl font-bold mb-6">{data.title}</h1>
 	<p class="text-xl font-semibold mb-4">{data.description}</p>
